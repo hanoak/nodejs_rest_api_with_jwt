d50c00fb52f24e1612e37aa8dab9b02a8f0ed3b2
Hanoak <hanoak4online@gmail.com>
2021-07-04 10:24:01 +0530

    done with app.js basic setup

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const authRoutes = require('./routes/auth');
+const crudRoutes = require('./routes/crud');
 
 const app = express();
+const MONGODBURL = "<your mongodb's cluster url here>";
+app.use(express.json());
 
+app.use((req, res, next) => {
 
-app.use('/', (req, res, next) => {
-    console.log('getting started.');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 });
 
+app.use(authRoutes);
+app.use(crudRoutes);
 
-app.listen(3000);
+app.use((error, req, res, next) => {
+
+    console.log(error);
+    res.status(error.statusCode || 500).json({
+        message: error.message,
+        data: error.data
+    });
+
+});
+
+mongoose.connect(MONGODBURL)
+    .then(result => {        
+        app.listen(3000);
+    })
+    .catch(error => {
+        console.log(error);
+    });
