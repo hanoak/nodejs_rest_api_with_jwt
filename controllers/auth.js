89a955c65c8a4a8eeb70729337ec962af6daf1d5
Hanoak <hanoak4online@gmail.com>
2021-07-06 12:14:41 +0530

    done with auth controller

---
+++ controllers/auth.js
@@ -0,0 +1,46 @@
+const {  } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+exports.getAuth = (req, res, next) => {
+
+    const email = req.body.email;
+    const password = req.body.password;
+    let dbuser;
+
+    User.findOne({email: email})
+        .then(user => {
+
+            if(! user) {
+                const err = new Error('Email not found!');
+                err.statusCode = 401;
+                throw err;
+            }
+            dbuser = user;
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual => {
+
+            if(! isEqual) {
+                const err = new Error('Wrong password!');
+                err.statusCode = 401;
+                throw err;
+            }
+
+            const token = jwt.sign({
+                uid: dbuser._id.toString(),                
+            }, 'YourSecretKeyGoesHere123', {
+                expiresIn: '1h'
+            });
+
+            res.status(500).json({token: token, uid: dbuser._id.toString()});
+        })
+        .catch(err => {
+            if(! err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+};
