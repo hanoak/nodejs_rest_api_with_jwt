ea3426c3fa0b6a0a0765d349bc8768f656da7df1
Hanoak <hanoak4online@gmail.com>
2021-07-06 12:14:41 +0530

    done with auth model

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+});
+
+module.exports = mongoose.model('User', userSchema);
